3e4a4d540554512f590bc7a73780eda0c7b34793
konstantinnovation <konstans@stuy.edu>
2025-10-23 09:11:37 -0400

    Add file compilation checks to runner.sh
    Added a loop to check for required Java files and compile them.

--- runner.sh
+++ runner.sh
@@ -9,7 +9,22 @@ RED="\033[0;31m"
 RESET="\033[0m"
 
 
-#ADD TESTS HERE
+################# FILES THAT NEED COMPILE - TRY TO COMPILE #############
+required_files=("SuperArray.java" "ValidateMethods.java" )
+for file in "${required_files[@]}"; do
+  if [ -f "$file" ]; then
+    echo -e "${GREEN}✅$file found${RESET}"
+    if javac "$file"; then
+      echo -e "${GREEN}✅$file compiled${RESET}"
+    else
+      echo -e "${RED}❌Failed to compile $file${RESET}"
+      err=1
+    fi
+  else
+    echo -e "${RED}❌$file does not exist.${RESET}"
+    err=1
+  fi
+done
 
 
 exit $err
